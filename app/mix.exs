ec1eab718bc5ddf83a1738097125d429e81f7fa2
Rustem Yusupov <rustem.yusupov@sigma.software>
2019-05-15 12:00:11 +0300

    New set up gor CircleCI

--- app/mix.exs
+++ app/mix.exs
@@ -11,7 +11,10 @@ defmodule Api.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      dialyzer: [plt_add_deps: :transitive],
+      dialyzer: [
+        plt_add_deps: :transitive,
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
       test_coverage: [tool: ExCoveralls]
     ]
   end
